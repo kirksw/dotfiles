6afe5f80eeb2a435b36e9c22098b2ef64c5f08c5
Kirk Sweeney <kirk.sweeney@LEGO.com>
2023-06-10 16:26:29 +0200

    clean up scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,52 @@
 #!/bin/zsh
 
-echo 'Unstowing common dirs'
-for PACKAGE in git nvim ranger rtx tmux wezterm zsh
-do
-    stow -D -v -d config -t $HOME $PACKAGE
-done
+echo '#####################################################'
+echo '## Un-stowing Config and Resource Files            ##'
+echo '#####################################################'
 
+## Link dotfiles
+if [ -f "/etc/arch-release" ]; then
+	echo 'Stowing arch configs'
+  pushd config-nix
+  for PACKAGE in $(ls -d */); do
+		stow -D --no-folding -t $HOME $PACKAGE
+	done
+  popd
+
+	echo 'Stowing *nix resources'
+  pushd resources
+  for PACKAGE in $(ls -d */); do
+		stow -D --no-folding -t $HOME $PACKAGE
+	done
+  popd
+fi
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+	echo 'Stowing macos configs'
+  pushd config-mac
+  for PACKAGE in $(ls -d */); do
+		stow -D --no-folding -t $HOME $PACKAGE
+	done
+  popd
+fi
+
+echo 'Stowing common configs'
+  pushd config
+  for PACKAGE in $(ls -d */); do
+    stow -D --no-folding -t $HOME $PACKAGE
+  done
+  popd
+
+echo 'Stowing private configs/resources'
+pushd private
+for PACKAGE in $(ls -d */); do
+  echo "stowing private package [$PACKAGE]"
+	stow -D --no-folding -t $HOME $PACKAGE 
+done
+popd
+
+echo '#####################################################'
+echo '## Un-stowing Config and Resource Files            ##'
+echo '#####################################################'
+
+echo 'Note: only the dotfiles were uninstalled, the tooling remains installed and would have to be manually removed.'
